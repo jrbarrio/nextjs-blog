c8e64941a2b9c78818ea6311a5a987c2b1370368
Jorge Roldan <jorge.roldan@gmail.com>
2023-05-27 00:00:27 +0200

    Adding gtag script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
     return (
@@ -7,10 +8,27 @@ export default function FirstPost() {
         <Head>
           <title>First Post</title>
         </Head>
+        
+        <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-FZNQKNCM4Q"
+        strategy="afterInteractive"
+        />
+        <div className="container">
+        <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FZNQKNCM4Q');
+        `}
+        </Script>
+        </div> 
+
         <h1>First Post</h1>
         <h2>
           <Link href="/">← Back to home</Link>
         </h2>
       </>
     );
-  }  
+  }
